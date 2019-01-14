32bcf2b169185991668911193c8a78a045d171b2
seeker <wildetangeant@gmail.com>
2019-01-14 14:15:09 -0800

    Got randomized eyes.  Now need to bring everything else into the mix

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function make_face() {
+  const eyes = [":","|"];
+  const nose = ["-",];
+  const mouth = ["(",")","|"];
+
+var item = eyes[Math.floor(Math.random()*eyes.length)];
+
+var nu_face = item
+alert(nu_face)
+}
