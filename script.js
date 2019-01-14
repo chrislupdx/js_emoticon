b134c1e0694c3acf6ed063cee60b2ae6e196dbfc
seeker <wildetangeant@gmail.com>
2019-01-14 15:02:45 -0800

    added some extra faces, made the title blue

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 function make_face() {
-  const eyes = [":","|"];
-  const nose = ["-",];
-  const mouth = ["(",")","|"];
+  const eyes = [":","|","="];
+  const nose = ["-","^","*"];
+  const mouth = ["(",")","|","[","]","p"];
 
 var fresh_eye = eyes[Math.floor(Math.random()*eyes.length)];
 var fresh_nose = nose[Math.floor(Math.random()*nose.length)];
 var fresh_mouth = mouth[Math.floor(Math.random()*mouth.length)];
 
 var nu_face = fresh_eye + fresh_nose + fresh_mouth;
+//render nu_face to the DOM?
 alert(nu_face)
 }
